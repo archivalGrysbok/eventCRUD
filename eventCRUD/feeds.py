70dd3e04ef31dc407d205f75c5658d80145d5820
cincrin <cincrin@gmail.com>
2011-02-26 17:48:00 -0500

    Fixed the spoiler issue

--- eventCRUD/feeds.py
+++ eventCRUD/feeds.py
@@ -1,16 +1,18 @@
 from django.contrib.syndication.views import Feed
+from datetime import datetime
 from models import Run
 
 class UpcomingRunsFeed(Feed):
     title = "Upcoming Larps"
-    link = "/sitenews/"
+    link = "/feed/"
     description = "LARPs scheduled to run in the future."
+    description_template = 'feeds/beat_description.html'
 
     def items(self):
         return Run.objects.filter(startdate__isnull=False).filter(startdate__gt=datetime.now).order_by("startdate")
 
     def item_title(self, item):
-        return item
+        return item.larp
 
     def item_description(self, item):
         return item.larp.description
